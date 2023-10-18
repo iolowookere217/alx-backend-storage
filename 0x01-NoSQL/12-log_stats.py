c2a41499acd8e2874e3442a4276557399e4ec7e1
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-18 00:39:26 +0000

    Mandatory tasks

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A Python script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["logs"]
+    col = db["nginx"]
+
+    logsCount= col.count_documents({})
+
+    print(f"{logCount} logs\nMethods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for items in methods:
+        methodCount = col.count_documents({"method": method})
+        print(f'\tmethod {method}: {methodCount}')
+
+    getStatusCount = col.count_documents({'method': 'GET',
+                                            'path': '/status'})
+
+    print(f"{getStatusCount} status check")
